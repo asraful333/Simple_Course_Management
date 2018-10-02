3edab9708771617ef9013d01958906a7b0a187ab
asraful333 <asrafulkarim333@gmail.com>
2018-10-02 23:20:57 +0600

    simple change

--- reg_print.php
+++ reg_print.php
@@ -57,20 +57,21 @@
 								</tr>
 								<tr>
 									<td>Cell</td>
-									<td><?php echo $res['cell']; ?></td>
+									<td>0<?php echo $res['cell']; ?></td>
 								</tr>
 								<tr>
 									<td>Address</td>
 									<td><?php echo $res['address']; ?></td>
 								</tr>
 								<tr>
-									<td>Batch ID</td>
-									<td><?php echo $res['batch_id']; ?></td>
-								</tr>
-								<tr>
 									<td>Course ID</td>
 									<td><?php echo $res['course_id']; ?></td>
 								</tr>
+								<tr>
+									<td>Batch ID</td>
+									<td><?php echo $res['batch_id']; ?></td>
+								</tr>
+
 
 							</table>
 							<div style="text-align: center;"><a class="btn btn-success hidden-print" onclick="window.print()" href="">Print</a>
