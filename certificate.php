0c8e4294f2e329d0f89c1b0f173f3fe2a03000be
asraful333 <asrafulkarim333@gmail.com>
2018-10-04 14:54:16 +0600

    simple design certificate

--- certificate.php
+++ certificate.php
@@ -10,61 +10,36 @@
 
 			<div class="col-md-6">
 				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h3>Certificate of Completion<span class="pull-right"><a class="btn btn-success hidden-print" href="student_info.php">Back</a></span></h3>
-					</div>
+					
 					<div class="panel-body">
-						<div style="max-width: 400px; margin: 0 auto;">
+						<div style="max-width: 400px; margin: 0 auto; border-radius: 2px solid black; text-align: center;">
 
-							<table class="table table-striped">
-								<tr>
-									<th>Title</th>
-									<th>Value</th>
-								</tr>
+							<h3 style="font-family: Roboto;">COURSE CERTIFICATE</h3>
+							<h4>This certifies that</h4><br/>
+
 
 								<?php
 									include 'inc/connect.php';
 					
 									$student_id= $_GET['student_id'];
 
-									$q = "SELECT * FROM student_reg WHERE student_id = $student_id ";
+									$q = "SELECT * FROM student_reg s INNER JOIN batch_tb b ON s.batch_id = b.batch_id INNER JOIN course_tb c ON b.course_id = c.course_id WHERE student_id= $student_id";
 									$query= mysqli_query($conn,$q);
 									$res= mysqli_fetch_assoc($query);
 									
 								?>
+																
+									<h3><b><?php echo $res['student_name']; ?></b></h3><br/>
 
-								<tr>
-									<td>Student ID</td>
-									<td><?php echo $res['student_id']; ?></td>
-								</tr>
-								<tr>
-									<td>Name</td>
-									<td><?php echo $res['student_name']; ?></td>
-								</tr>
-								<tr>
-									<td>Cell</td>
-									<td>0<?php echo $res['cell']; ?></td>
-								</tr>
-								<tr>
-									<td>Address</td>
-									<td><?php echo $res['address']; ?></td>
-								</tr>
-								<tr>
-									<td>Course ID</td>
-									<td><?php echo $res['course_id']; ?></td>
-								</tr>
-								<tr>
-									<td>Batch ID</td>
-									<td><?php echo $res['batch_id']; ?></td>
-								</tr>
-								<tr>
-									<td>Result</td>
-									<td><?php echo $res['result']; ?></td>
-								</tr>
+									<h4>has successfully completed</h4>
+														
+									<h3>Course Name: <?php echo $res['course_name']; ?></h3>
+								
+									<h3>Batch Name: <?php echo $res['batch_name']; ?></h3>
+								
+									<h3>Result: <?php echo $res['result']; ?></h3>
 
 
-
-							</table>
 							<div style="text-align: center;"><a class="btn btn-success hidden-print" onclick="window.print()" href="">Print</a>
 							</div>
 							
