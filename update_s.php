bf9a1e9f1d7858b9e721321f2305d30752e8943a
asraful333 <asrafulkarim333@gmail.com>
2018-10-04 15:37:23 +0600

    student name update when new name insert in the same batch

--- update_s.php
+++ update_s.php
@@ -6,18 +6,29 @@
 
 	if (isset($_POST['submit'])) {
 
-	$student_id	=$_GET['student_id'];
+		$student_id	=$_GET['student_id'];
 
-	$name 		= $_POST['name'];
-	$cell 		= $_POST['cell'];
-	$address 	= $_POST['address'];
-	$result 	= $_POST['result'];
+		$name 		= mysqli_real_escape_string($conn,$_POST['name']);
+		$cell 		= mysqli_real_escape_string($conn,$_POST['cell']);
+		$address 	= mysqli_real_escape_string($conn,$_POST['address']);
+		$course_id 	= mysqli_real_escape_string($conn,$_POST['course_id']);
+		$batch_id 	= mysqli_real_escape_string($conn,$_POST['batch_id']);
+		$result 	= mysqli_real_escape_string($conn,$_POST['result']);
 
-	$sql= "UPDATE student_reg SET student_id=$student_id, student_name='$name', cell='$cell', address='$address', result='$result' WHERE student_id=$student_id";
+		$sql= "SELECT * FROM student_reg WHERE student_name='$name' AND course_id='$course_id' AND batch_id='$batch_id' AND student_id!='$student_id'";
+		$res_s= mysqli_query($conn,$sql) or die(mysqli_error($conn));
 
-	$qry= mysqli_query($conn,$sql);
+		if (mysqli_num_rows($res_s)>0) {
+			$msg= "Student name is exists under the batch!";
+		}
+		else{
 
-	header("location: student_info.php");
+			$sql= "UPDATE student_reg SET student_id=$student_id, student_name='$name', cell='$cell', address='$address', result='$result' WHERE student_id=$student_id";
+
+			$qry= mysqli_query($conn,$sql);
+
+			header("location: student_info.php");
+			}
 	}
 
 	
@@ -38,21 +49,41 @@
 						<div style="max-width: 400px; margin: 0 auto;">
 
 							<form action="" method="POST">
+
+								<?php
+					
+
+									$student_id	=$_GET['student_id'];
+									$q = "SELECT * FROM student_reg WHERE student_id = $student_id ";
+									$query= mysqli_query($conn,$q);
+									$res= mysqli_fetch_assoc($query);
+									
+								?>
+
 								<div class="form-group">
 									<label for="name">Full Name: </label>
-									<input type="text" name="name" id="name" class="form-control" required="" />
+									<input type="text" name="name" id="name" class="form-control" required="" value="<?php echo $res['student_name']; ?>" />
+									<?php if (isset($msg)): ?>
+									<span><?php echo $msg;?></span>
+								<?php endif ?>
 								</div>								
 								<div class="form-group">
 									<label for="cell">Cell: </label>
-									<input type="text" name="cell" id="cell" class="form-control" required="" />
+									<input type="text" name="cell" id="cell" class="form-control" required="" value="<?php echo $res['cell']; ?>"/>
 								</div>
 								<div class="form-group">
 									<label for="address">Address: </label>
-									<textarea type="text" name="address" id="address" class="form-control" rows="3" required=""></textarea>
+									<textarea type="text" name="address" id="address" class="form-control" rows="3" required="" ><?php echo $res['address']; ?></textarea>
+								</div>
+								<div class="form-group">									
+									<input type="hidden" name="course_id" id="course_id" class="form-control" required="" value="<?php echo $res['course_id']; ?>"/>
+								</div>
+								<div class="form-group">
+									<input type="hidden" name="batch_id" id="batch_id" class="form-control" required="" value="<?php echo $res['batch_id']; ?>"/>
 								</div>
 								<div class="form-group">
 									<label for="result">Result: </label>
-									<input type="text" name="result" id="result" class="form-control" required="" />
+									<input type="text" name="result" id="result" class="form-control" required="" value="<?php echo $res['result']; ?>"/>
 								</div>
 								
 								<button type="submit" name="submit" class="btn btn-success">Submit</button>
