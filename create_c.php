0ff5c46f432c79a4af090b957e1cbe6757802a90
asraful333 <asrafulkarim333@gmail.com>
2018-10-04 15:00:11 +0600

    validation and special character assign

--- create_c.php
+++ create_c.php
@@ -6,17 +6,26 @@
 
 	if (isset($_POST['submit'])) {
 
-		$course_n = $_POST['course_n'];
+
+		$course_n = mysqli_real_escape_string($conn,$_POST['course_n']);
 		$type = $_POST['type'];
 		
+		$sql= "SELECT * FROM course_tb WHERE course_name='$course_n'";
+		$res_s= mysqli_query($conn,$sql) or die(mysqli_error($conn));
 
-		$qr = "INSERT INTO `course_tb`(`course_name`, `course_type`) VALUES ('$course_n','$type')";
+		if (mysqli_num_rows($res_s)>0) {
+			$msg= "Course is already exists";
+		}
+		else{
 
-		$qry = mysqli_query($conn,$qr);
-		if ($qry) {
-			header('Location: admin_profile.php');
-		}else{
-			echo "Insert Failed!..";
+			$qr = "INSERT INTO `course_tb`(`course_name`, `course_type`) VALUES ('$course_n','$type')";
+
+			$qry = mysqli_query($conn,$qr);
+			if ($qry) {
+				header('Location: admin_profile.php');
+			}else{
+				echo "Insert Failed!..";
+			}
 		}
 
 	}
@@ -24,7 +33,7 @@
 		include 'inc/header1.php';
 		include 'inc/adminnav.php';
 ?>
-				
+			
 
 			<div class="col-md-8">
 				<div class="panel panel-default">
@@ -38,6 +47,9 @@
 								<div class="form-group">
 									<label for="course_n">Course Name: </label>
 									<input type="text" name="course_n" id="course_n" class="form-control" required="" />
+									<?php if (isset($msg)): ?>
+									<span><?php echo $msg;?></span>
+								<?php endif ?>
 								</div>
 								<div class="form-group">
 									<label for="type">Type:</label>
